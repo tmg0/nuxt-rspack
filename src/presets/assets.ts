77c42a8427451329ac8c14dde1caea15d8a04246
Tamago <zekun.jin@foxmail.com>
2024-07-02 16:05:33 +0800

    feat: use buildin assets rule

--- src/presets/assets.ts
+++ src/presets/assets.ts
@@ -3,22 +3,8 @@ import type { RspackConfigContext } from '../utils/config'
 export function assets(ctx: RspackConfigContext) {
   ctx.config.module!.rules!.push(
     {
-      test: /\.(png|jpe?g|gif|svg|webp)$/i,
-      use: [{
-        loader: 'url-loader',
-      }],
-    },
-    {
-      test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
-      use: [{
-        loader: 'url-loader',
-      }],
-    },
-    {
-      test: /\.(webm|mp4|ogv)$/i,
-      use: [{
-        loader: 'file-loader',
-      }],
+      test: /\.(png|jpe?g|gif)$/i,
+      type: 'asset/resource',
     },
   )
 }
